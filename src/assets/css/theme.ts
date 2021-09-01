e54b467358b30a0e2e966a75626b0cc75c8451ef
JaFe <kontakt.jakubfedoszczak@gmail.com>
2021-09-01 15:47:26 +0200

    Modify GlobalStyles and add one color in theme
    New color: light black

--- src/assets/css/theme.ts
+++ src/assets/css/theme.ts
@@ -4,7 +4,8 @@ export const theme = {
     blueDark: '#5E6BFF',
     blue: '#8689FF',
     blueLight: '#ACA8FF',
-    blueLighten: '#D1C8FF'
+    blueLighten: '#D1C8FF',
+    blackLight: 'rgba(0, 0, 0, 70%)'
   },
   shadows: {
     primary: '0 5px 15px -7px #000000'
@@ -19,6 +20,7 @@ export interface themeProps {
       blue: string;
       blueLight: string;
       blueLighten: string;
+      blackLight: string;
     };
     shadows: {
       primary: string;
