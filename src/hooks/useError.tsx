c5856a6b5e1296e93a9a2f7536d60297d50e72fb
JaFe <kontakt.jakubfedoszczak@gmail.com>
2021-09-03 17:12:52 +0200

    Add global error handling hook

---
+++ src/hooks/useError.tsx
@@ -0,0 +1,38 @@
+import React, { createContext, useState, useContext } from 'react';
+
+interface contextProps {
+  error: string | null;
+  dispatchError: (message: string) => void;
+}
+
+const ErrorContext = createContext<contextProps>({
+  error: 'Default error',
+  dispatchError: (message: string) => console.error(message)
+});
+const ErrorProvider: React.FC = ({ children }) => {
+  const [error, setError] = useState<null | string>(null);
+
+  const dispatchError = (message: string): void => {
+    setError(message);
+    setTimeout(() => {
+      setError(null);
+    }, 6000);
+  };
+
+  const value: contextProps = {
+    error,
+    dispatchError
+  };
+
+  return <ErrorContext.Provider value={value}>{children}</ErrorContext.Provider>;
+};
+
+export const useError = (): contextProps => {
+  const error = useContext(ErrorContext);
+
+  if (!error) console.error('Something went wrong with error handling. Please contact with support!');
+
+  return error;
+};
+
+export default ErrorProvider;
