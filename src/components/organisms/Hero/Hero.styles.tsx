344ecff33ecee558e4ca96027a15760d40716367
JaFe <kontakt.jakubfedoszczak@gmail.com>
2021-09-09 05:40:35 +0200

    Repair Hero component responsibility

--- src/components/organisms/Hero/Hero.styles.tsx
+++ src/components/organisms/Hero/Hero.styles.tsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { Content } from 'components/atoms/Content/Content';
 
 export const Wrapper = styled.div`
   min-height: calc(100vh - 200px);
@@ -8,12 +9,35 @@ export const Wrapper = styled.div`
   flex-direction: column;
   text-align: center;
   padding-inline: 35px;
+  margin-bottom: 60px;
   @media (min-width: 768px) {
     position: relative;
     align-items: flex-end;
+    padding-right: 70px;
+    text-align: right;
+    margin-bottom: 0;
+    h2 {
+      font-size: 70px;
+    }
+    h3 {
+      font-size: 25px;
+    }
+    p {
+      font-size: 18px;
+    }
+  }
+  @media (min-width: 1100px) {
     padding-right: 200px;
     padding-left: 50px;
-    text-align: right;
+    h2 {
+      font-size: 90px;
+    }
+    h3 {
+      font-size: 30px;
+    }
+    p {
+      font-size: 25px;
+    }
   }
 `;
 
@@ -27,5 +51,14 @@ export const Image = styled.img`
     top: 50%;
     width: 600px;
     transform: scaleX(-1) translateY(-50%);
+  }
+`;
+
+export const StyledContent = styled(Content)`
+  @media (min-width: 768px) {
+    width: 70%;
+  }
+  @media (min-width: 768px) {
+    width: 50%;
   }
 `;
--- src/components/organisms/Hero/Hero.tsx
+++ src/components/organisms/Hero/Hero.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
-import { Wrapper, Image } from './Hero.styles';
+import { Wrapper, Image, StyledContent } from './Hero.styles';
 import Dog from 'assets/img/dog.jpg';
 import { Title } from 'components/atoms/Title/Title';
 import { SubTitle } from 'components/atoms/SubTitle/SubTitle';
-import { Content } from 'components/atoms/Content/Content';
 import { Button } from 'components/atoms/Button/Button';
 import { useHistory } from 'react-router';
 
@@ -17,9 +16,7 @@ const Hero: React.FC = () => {
         Ratujemy zwierzęta <br /> w potrzebie
       </Title>
       <SubTitle>Kochasz zwierzątka? To miejsce dla ciebie!</SubTitle>
-      <Content>
-        Zerknij na członków naszej rodziny i jeżeli to możliwe przygarnij <br /> kogoś do swojego domu!
-      </Content>
+      <StyledContent>Zerknij na członków naszej rodziny i jeżeli to możliwe przygarnij kogoś do swojego domu!</StyledContent>
       <Button onClick={() => history.push('/adoption')}>Zobacz naszą rodzinę</Button>
     </Wrapper>
   );
