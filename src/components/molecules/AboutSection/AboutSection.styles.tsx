323f225db7a263d81a926ae857c88d5d825c3319
JaFe <kontakt.jakubfedoszczak@gmail.com>
2021-09-14 19:06:43 +0200

    Add next mobile version of AboutSection (>720px)

--- src/components/molecules/AboutSection/AboutSection.styles.tsx
+++ src/components/molecules/AboutSection/AboutSection.styles.tsx
@@ -16,9 +16,15 @@ export const Header = styled.div`
   justify-content: center;
   flex-direction: column;
   padding: 65px 0 90px 0;
+  @media (min-width: 768px) {
+    padding: 75px 0 100px 0;
+  }
 `;
 export const StyledTitle = styled(Title)`
   color: white;
+  @media (min-width: 768px) {
+    font-size: 60px;
+  }
 `;
 export const StyledSubTitle = styled(SubTitle)`
   margin-top: -8px;
@@ -26,6 +32,10 @@ export const StyledSubTitle = styled(SubTitle)`
   opacity: 0.8;
   letter-spacing: 1px;
   font-weight: 400;
+  @media (min-width: 768px) {
+    font-size: 30px;
+    margin-top: -20px;
+  }
 `;
 export const ImageWrapper = styled.div`
   height: 600px;
@@ -34,6 +44,9 @@ export const ImageWrapper = styled.div`
   position: relative;
   box-shadow: ${({ theme }: themeProps) => theme.shadows.primary};
   overflow: hidden;
+  @media (min-width: 768px) {
+    height: 700px;
+  }
 `;
 export const Image = styled.img`
   min-height: 100%;
@@ -53,11 +66,19 @@ export const Content = styled.div`
   width: 80%;
   height: 620px;
   padding: 30px 20px;
+  @media (min-width: 768px) {
+    top: 210px;
+    width: 85%;
+    height: 720px;
+  }
 `;
 export const InnerContent = styled.div`
   height: 100%;
   background-color: white;
   padding: 12px;
+  @media (min-width: 768px) {
+    font-size: 20px;
+  }
 `;
 export const StyledParagraph = styled(Paragraph)`
   text-align: justify;
@@ -66,4 +87,7 @@ export const StyledParagraph = styled(Paragraph)`
   -webkit-line-clamp: 27;
   -webkit-box-orient: vertical;
   overflow: hidden;
+  @media (min-width: 768px) {
+    line-height: 1.5;
+  }
 `;
