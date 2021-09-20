3b9360c570375f71ce6e184078254d41f1923091
JaFe <kontakt.jakubfedoszczak@gmail.com>
2021-09-20 18:49:23 +0200

    Add SiteTitle component

---
+++ src/components/molecules/SiteTitle/SiteTitle.styles.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+import { Title } from 'components/atoms/Title/Title';
+import { SubTitle } from 'components/atoms/SubTitle/SubTitle';
+
+export const Wrapper = styled.div`
+  width: 100%;
+  text-align: center;
+`;
+export const StyledTitle = styled(Title)`
+  margin: 0;
+`;
+export const StyledSubTitle = styled(SubTitle)``;
