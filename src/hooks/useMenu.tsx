b0d5c4332465a953cbf3942bb79c91e9b484ec34
JaFe <kontakt.jakubfedoszczak@gmail.com>
2021-09-28 18:55:20 +0200

    Extract menu service to useMenu hook

---
+++ src/hooks/useMenu.tsx
@@ -0,0 +1,66 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import React, { createContext, useContext } from 'react';
+import { useError } from 'hooks/useError';
+import { gsap } from 'gsap';
+
+interface ContextProps {
+  turnOn: (links: React.RefObject<HTMLUListElement>, indicator: React.RefObject<HTMLDivElement>) => void;
+}
+
+const MenuContext = createContext<ContextProps>({
+  turnOn: (links: React.RefObject<HTMLUListElement>, indicator: React.RefObject<HTMLDivElement>) => console.log('Ready!')
+});
+const MenuProvider: React.FC = ({ children }) => {
+  const getPositionOf = (el: any) => {
+    const { width: itemWidth, height: itemHeight, x: itemX, y: itemY } = el.getBoundingClientRect();
+    const currentWidth = itemWidth * 0.6;
+    const readyX = itemX + itemWidth / 2 - currentWidth / 2;
+    const readyY = itemY + itemHeight + 7;
+
+    return { readyX, readyY, currentWidth };
+  };
+  const findActiveElement = (links: React.RefObject<HTMLUListElement>) => {
+    if (links.current) {
+      return document.querySelector('.active-link');
+    }
+  };
+  const setInitialPosition = (links: React.RefObject<HTMLUListElement>, indicator: React.RefObject<HTMLDivElement>) => {
+    if (links.current && indicator.current) {
+      const { readyX, readyY, currentWidth } = getPositionOf(findActiveElement(links));
+      gsap.set(indicator.current, { x: readyX, y: readyY, width: currentWidth });
+    }
+  };
+  const goToThisElement = (el: any, indicator: React.RefObject<HTMLDivElement>, links: React.RefObject<HTMLUListElement>) => {
+    if (links.current && indicator.current) {
+      const { readyX, readyY, currentWidth } = getPositionOf(el);
+      gsap.to(indicator.current, { x: readyX, y: readyY, duration: 0.8, width: currentWidth });
+    }
+  };
+  const activateTab = (e: any, indicator: React.RefObject<HTMLDivElement>, links: React.RefObject<HTMLUListElement>) => {
+    if (e.target.classList.contains('no-active')) {
+      goToThisElement(e.target, indicator, links);
+    }
+  };
+  const turnOn = (links: React.RefObject<HTMLUListElement>, indicator: React.RefObject<HTMLDivElement>) => {
+    setInitialPosition(links, indicator);
+    if (links.current) {
+      links.current.addEventListener('click', (e) => activateTab(e, indicator, links));
+    }
+  };
+
+  const value: ContextProps = {
+    turnOn
+  };
+
+  return <MenuContext.Provider value={value}>{children}</MenuContext.Provider>;
+};
+
+export const useMenu = (): ContextProps => {
+  const { dispatchError } = useError();
+
+  const menu = useContext(MenuContext);
+  if (!menu) dispatchError('Something went wrong with Menu! Please, contact with us!');
+  return menu;
+};
+
+export default MenuProvider;
